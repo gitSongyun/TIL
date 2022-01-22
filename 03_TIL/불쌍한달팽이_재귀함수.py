79626edb00395886bbac95270d4c51df74557642
gitSongyun <baesongyun@gmail.com>
2022-01-23 01:16:19 +0900

    reculsive

---
+++ 03_TIL/불쌍한달팽이_재귀함수.py
@@ -0,0 +1,23 @@
+def snail(height, day, night, cnt =1):
+        # 재귀 종료 지점 생성
+        if height <= day:
+            return cnt
+        # 재귀 생성
+        else: 
+            
+            height -= (day-night)
+            return snail(height,day,night, cnt+1)
+        
+print(snail(150, 6, 3))
+
+# 총장님 코드 (기밀)
+def snail(height, day, night, cnt=1):
+    height -= day
+
+    if height <= 0:
+        return cnt
+
+    height += night
+
+    return snail(height, day, night, cnt+1)
+print(snail(100, 5, 2))
