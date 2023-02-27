9e81141e199f06b85cf2422598bc63437e3d4e5b
songyunbae <baesongyun@gmail.com>
2023-02-27 22:59:37 +0900

    프로그래머스 - 섬 연결하기 (그리디,크루스칼)

---
+++ 04_Algorithm/프로그래머스/Python/탐욕법_문제집/섬 연결하기(크루스칼)/s1.py
@@ -0,0 +1,26 @@
+def solution(n, costs):
+    answer = 0
+    uni = [0] * (n + 1)
+    costs.sort(key = lambda x: x[2])
+    for i in range(1,n + 1):
+        uni[i] = i
+        
+    def union(x,y):
+        a = find(x)
+        b = find(y)
+        if a != b:
+            uni[a] = b
+    
+    def find(x):
+
+        if uni[x] == x: return x
+        else:
+            uni[x] = find(uni[x])
+            return uni[x]
+    
+    for n1,n2,cost in costs:
+        print(f'{n1}을 보냈을때')
+        if find(n1) != find(n2):
+            union(n1,n2)
+            answer += cost
+    return answer
