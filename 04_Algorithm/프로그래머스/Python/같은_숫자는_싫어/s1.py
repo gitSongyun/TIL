a49d71f2e55ff783cadb2029390049fd16a1befb
songyunbae <baesongyun@gmail.com>
2023-03-01 23:23:22 +0900

    프로그래머스- 같은 숫자는 싫어

---
+++ 04_Algorithm/프로그래머스/Python/같은_숫자는_싫어/s1.py
@@ -0,0 +1,11 @@
+from collections import Counter
+def solution(arr):
+    
+    tmp = arr[0]
+    answer = [tmp]
+    for a in arr:
+        if tmp != a:
+            tmp = a
+            answer.append(tmp)
+
+    return answer
