3055299d84a32f52f5fde82ba9662f2393d93ef1
songyunbae <baesongyun@gmail.com>
2023-05-15 23:30:30 +0900

    BOJ - 주사위윷놀이

--- 04_Algorithm/SWEA/연습장/순열,조합연습.py
+++ 04_Algorithm/SWEA/연습장/순열,조합연습.py
@@ -1,22 +1,20 @@
+
+
 def pick(n, picked, topick):
-
     if topick == 0:
-        return picked
+        v = list()
+        for p in picked:
+            v.append(number[p])
+        lst.append(v)
 
-    if not picked:
-        smallest = 0
-    else:
-        smallest = picked[-1]
-
+    smallest = 0 if not picked else picked[-1]
     for i in range(smallest, n):
-
         if i not in picked:
-            picked.append(i)
-            pick(n, picked, topick-1)
-            picked.pop()
+            pick(n, picked + [i], topick - 1)
 
 
 
+# 조합
 def pick(n, picked, topick):
     if topick == 0:
         v = list()
@@ -30,25 +28,53 @@ def pick(n, picked, topick):
         if i not in picked:
             pick(n, picked + [i], topick - 1)
 
+# 순열
+# def pick(n, picked, topick):
+#     if topick == 0:
+#         v = list()
+#         for p in picked:
+#             v.append(number[p])
+#         lst.append(v)
+#         return
+#
+#
+#     for i in range(n):
+#         if i not in picked:
+#             pick(n, picked + [i], topick - 1)
 
 lst = []
-number = [1, 1, 9]
-pick(3, [], 2)
+number = [1, 2, 9, 5]
+pick(4, [], 2)
 print(lst)
 
+l = ['a', 'a', 'c', 'd']
+n = len(l)
+r = 2
+answer = []
 
-def pick(n, picked, topick):
-    if topick == 0:
-        ans = picked[:]
-        return ans
+def dfs(idx, list):
+    if len(list) == r:
+        answer.append(list[:])
+        return
 
-    if not picked:
-        smallest = 0
-    else:
-        smallest = picked[-1]
+    for i in range(idx, n):
+        dfs(i+1,list+[l[i]])
 
-    for i in range(smallest, n):
-        if i not in picked:
-            picked.append(i)
-            pick(n, picked, topick -1)
-            picked.pop()
+dfs(0, [])
+print(answer)
+
+# def pick(n, picked, topick):
+#     if topick == 0:
+#         ans = picked[:]
+#         return ans
+#
+#     if not picked:
+#         smallest = 0
+#     else:
+#         smallest = picked[-1]
+#
+#     for i in range(smallest, n):
+#         if i not in picked:
+#             picked.append(i)
+#             pick(n, picked, topick -1)
+#             picked.pop()
